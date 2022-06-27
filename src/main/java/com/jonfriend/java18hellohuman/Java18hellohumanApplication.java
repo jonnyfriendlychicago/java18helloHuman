5fc910ae65dac6e1b10995f9541c31a6381055ab
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-06-27 11:56:17 -0500

    first commit

---
+++ src/main/java/com/jonfriend/java18hellohuman/Java18hellohumanApplication.java
@@ -0,0 +1,13 @@
+package com.jonfriend.java18hellohuman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java18hellohumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java18hellohumanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonfriend/java18hellohuman/Java18hellohumanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonfriend.java18hellohuman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java18hellohumanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
